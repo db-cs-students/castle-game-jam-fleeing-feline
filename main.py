e5d4ef2dc36a54f5dbd0daeeceb3bb24b3823a0b
Mei H <69876188+Mei606@users.noreply.github.com>
2020-09-28 10:21:28 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 """
-Title:
-Creators:
-Description:
+Title: Fleeing Feline
+Creators: Mei and Caroline
+Description: A cat trying to escape their castle and dodge various enemies
 """
+
+# game setup
+
+# player setup
+
+# Tilemap 
+
+# powerup setup
+
+# enemies setup
+
+# win and lose effect
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /** 
-Title:
-Creators:
-Description:
+Title: Fleeing Feline
+Creators: Mei and Caroline
+Description: A cat trying to escape their castle and dodge various enemies
 
  */
