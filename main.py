e012fdff18cb0ac5d326f0c456cdea00cb0b40a5
Mei H <69876188+Mei606@users.noreply.github.com>
2020-09-30 10:07:42 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,41 +5,6 @@ Description: A cat trying to escape their castle and dodge various enemies
 """
 
 # game setup
-
-# player setup
-cat = sprites.create(img("""
-    . . . . . . . . . . . . . .
-    e e e . . . . e e e . . . .
-    c d d c . . c d d c . . . .
-    c b d d f f d d b c . . . .
-    c 3 b d d b d b 3 c . . . .
-    f b 3 d d d d 3 b f . . . .
-    e d d d d d d d d e . . . .
-    e d f d d d d f d e b f b .
-    f d d f d d f d d f f d f .
-    f b d d b b d d 2 b f d f .
-    . f 2 2 2 2 2 2 d b d b f .
-    . f d d d d d d d f f f . .
-    . f d b d f f f d f . . . .
-    . . f f f f . . f f . . . .
-"""), SpriteKind.player)
-scene.camera_follow_sprite(cat)
-cat.ay = 300
-
-#Player controls
-def on_event_pressed():
-    if controller.right.is_pressed():
-        cat.vx = 100
-controller.right.on_event(ControllerButtonEvent.PRESSED, on_event_pressed)
-# Player controls
-def on_event_pressed2():
-    cat.vy = -150
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_event_pressed2)
-# def right():
-#     cat.vx = 100
-# controller.right.on_event(ControllerButtonEvent.PRESSED, right)
-
-# Tilemap 
 scene.set_background_image(img("""
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
@@ -162,35 +127,83 @@ scene.set_background_image(img("""
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
 """))
+# player setup
+cat = sprites.create(img("""
+    . . . . . . . . . . . . . .
+    e e e . . . . e e e . . . .
+    c d d c . . c d d c . . . .
+    c b d d f f d d b c . . . .
+    c 3 b d d b d b 3 c . . . .
+    f b 3 d d d d 3 b f . . . .
+    e d d d d d d d d e . . . .
+    e d f d d d d f d e b f b .
+    f d d f d d f d d f f d f .
+    f b d d b b d d 2 b f d f .
+    . f 2 2 2 2 2 2 d b d b f .
+    . f d d d d d d d f f f . .
+    . f d b d f f f d f . . . .
+    . . f f f f . . f f . . . .
+"""), SpriteKind.player)
+scene.camera_follow_sprite(cat)
+cat.ay = 300
+
+#Player controls
+controller.move_sprite(cat, 100, 0)
+
+def jump_up():
+    cat.vy = -140
+controller.A.on_event(ControllerButtonEvent.PRESSED, jump_up)
+
+
+
+# Tilemap 
 scene.set_tile_map(img("""
-    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-    .....................6..........................
-    .....................6..........................
-    .....................6..........................
-    ....ee....ee.........6..........................
-    ....ee....ee....................................
-    ................................................
-    ..eeeee22ee.....................................
-    ..eeeee22ee.....................................
-    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    ...................6......................6....................................b
+    ...................6......................6.....................................
+    ...................6......................6.....................................
+    ....ee....ee.......6.e..11..e......88888..6.....................................
+    ..............33...6.e..11..e......88888..6.....................................
+    ..........4...33..............99...88888........................................
+    ...eeee22ee...33.......eeee...99...88888........................................
+    ...eeee22ee...33.......eeee...99...88888.......................................b
+    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 """))
 scene.set_tile(11, img("""
-    7 6 7 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 7 6 6 6 6 6 6 6 6 6 6
-    6 6 6 7 6 7 7 6 6 6 6 6 7 6 6 6
-    6 6 8 7 7 6 7 6 7 7 6 6 6 6 6 6
-    6 6 6 8 7 6 6 7 7 8 6 6 6 6 6 6
-    6 6 6 6 8 6 6 7 8 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 7 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 7 7 6 6 6
-    6 6 6 6 6 6 6 6 6 6 7 7 8 6 6 6
-    6 6 6 6 6 6 6 6 7 7 6 8 6 6 6 6
-    6 6 6 6 6 6 6 6 6 7 7 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 7
-    6 6 7 6 6 6 6 6 6 6 6 6 6 6 6 7
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+"""), True)
+scene.set_tile(14, img("""
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
 """), True)
 
 # powerup setup
--- main.ts
+++ main.ts
@@ -5,40 +5,6 @@ Description: A cat trying to escape their castle and dodge various enemies
 
  */
 //  game setup
-//  player setup
-let cat = sprites.create(img`
-    . . . . . . . . . . . . . .
-    e e e . . . . e e e . . . .
-    c d d c . . c d d c . . . .
-    c b d d f f d d b c . . . .
-    c 3 b d d b d b 3 c . . . .
-    f b 3 d d d d 3 b f . . . .
-    e d d d d d d d d e . . . .
-    e d f d d d d f d e b f b .
-    f d d f d d f d d f f d f .
-    f b d d b b d d 2 b f d f .
-    . f 2 2 2 2 2 2 d b d b f .
-    . f d d d d d d d f f f . .
-    . f d b d f f f d f . . . .
-    . . f f f f . . f f . . . .
-`, SpriteKind.Player)
-scene.cameraFollowSprite(cat)
-cat.ay = 300
-// Player controls
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_event_pressed() {
-    if (controller.right.isPressed()) {
-        cat.vx = 100
-    }
-    
-})
-//  Player controls
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_event_pressed2() {
-    cat.vy = -150
-})
-//  def right():
-//      cat.vx = 100
-//  controller.right.on_event(ControllerButtonEvent.PRESSED, right)
-//  Tilemap 
 scene.setBackgroundImage(img`
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
@@ -161,33 +127,76 @@ scene.setBackgroundImage(img`
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
     5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
 `)
+//  player setup
+let cat = sprites.create(img`
+    . . . . . . . . . . . . . .
+    e e e . . . . e e e . . . .
+    c d d c . . c d d c . . . .
+    c b d d f f d d b c . . . .
+    c 3 b d d b d b 3 c . . . .
+    f b 3 d d d d 3 b f . . . .
+    e d d d d d d d d e . . . .
+    e d f d d d d f d e b f b .
+    f d d f d d f d d f f d f .
+    f b d d b b d d 2 b f d f .
+    . f 2 2 2 2 2 2 d b d b f .
+    . f d d d d d d d f f f . .
+    . f d b d f f f d f . . . .
+    . . f f f f . . f f . . . .
+`, SpriteKind.Player)
+scene.cameraFollowSprite(cat)
+cat.ay = 300
+// Player controls
+controller.moveSprite(cat, 100, 0)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function jump_up() {
+    cat.vy = -140
+})
+//  Tilemap 
 scene.setTileMap(img`
-    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-    .....................6..........................
-    .....................6..........................
-    .....................6..........................
-    ....ee....ee.........6..........................
-    ....ee....ee....................................
-    ................................................
-    ..eeeee22ee.....................................
-    ..eeeee22ee.....................................
-    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    ...................6......................6....................................b
+    ...................6......................6.....................................
+    ...................6......................6.....................................
+    ....ee....ee.......6.e..11..e......88888..6.....................................
+    ..............33...6.e..11..e......88888..6.....................................
+    ..........4...33..............99...88888........................................
+    ...eeee22ee...33.......eeee...99...88888........................................
+    ...eeee22ee...33.......eeee...99...88888.......................................b
+    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 `)
 scene.setTile(11, img`
-    7 6 7 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 7 6 6 6 6 6 6 6 6 6 6
-    6 6 6 7 6 7 7 6 6 6 6 6 7 6 6 6
-    6 6 8 7 7 6 7 6 7 7 6 6 6 6 6 6
-    6 6 6 8 7 6 6 7 7 8 6 6 6 6 6 6
-    6 6 6 6 8 6 6 7 8 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 7 6 6 6 6 6 6 6 6 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 7 7 6 6 6
-    6 6 6 6 6 6 6 6 6 6 7 7 8 6 6 6
-    6 6 6 6 6 6 6 6 7 7 6 8 6 6 6 6
-    6 6 6 6 6 6 6 6 6 7 7 6 6 6 6 6
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 7
-    6 6 7 6 6 6 6 6 6 6 6 6 6 6 6 7
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    d 1 d d d d d d d 1 d d d d d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    d d d d d 1 d d d d d d d 1 d d
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+`, true)
+scene.setTile(14, img`
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
+    e e e e e e e e e e e e e e e e
 `, true)
